6247b667496240b0d001d94bd83ae8ea6b3cb4ba
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-08-07 05:36:33 +0000

    Itemモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_082842) do
+ActiveRecord::Schema.define(version: 2021_08_07_053524) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.text "caption"
+    t.integer "tax_out_price"
+    t.string "image_id"
+    t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
