855dfa47e145ece04058e70b5b6ddbff24def79b
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-08-09 11:14:21 +0000

    モデル名を修正して、ShippingAddressモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_110700) do
+ActiveRecord::Schema.define(version: 2021_08_09_111325) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email"
@@ -54,6 +54,15 @@ ActiveRecord::Schema.define(version: 2021_08_09_110700) do
     t.integer "status"
     t.integer "address_option"
     t.integer "customer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shipping_addresses", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "name"
+    t.string "postal_code"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
