c67cb99501dd46b09acf61934774ed21896083a5
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-08-12 07:08:23 +0000

    Adminモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_071218) do
-
-  create_table "admins", force: :cascade do |t|
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2021_08_12_063546) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "customer_id"
