91ab16de588cc466f630e4b89315924adae3ec46
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-08-17 06:52:45 +0000

    deviseルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  devise_for :customers, controllers: {
+        sessions: 'customer/sessions',
+        registrations: 'customer/registrations'
+      }
+      
+  devise_for :admins, controllers: {
+        sessions: 'admin/sessions'
+      }
   
   
+  namespace :admin do
+    root to: "homes#top"
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:index, :show, :update]
+    resources :ordered_items, only: [:update]
+  end
 end
