d56e806ee07491bc18da2669458cdb83e995a754
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-08-24 10:20:39 +0000

    genresコントローラーeditアクション編集

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -17,8 +17,16 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
   end
+  
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+  
 end
