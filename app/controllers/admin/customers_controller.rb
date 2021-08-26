2f8ed19f731033518188840d1436d4376b5b370d
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-08-26 00:55:44 +0000

    会員一覧画面作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,16 +1,24 @@
 class Admin::CustomersController < ApplicationController
   
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
   end
 
+  private
+  
+  def customer_params
+   params.require(:customer).permit(:first_name,:last_name,:first_name_kana,:last_name_kana,:email,:postal_code,:address,:telphone_number,:is_deleted)
+  end
 
 end
