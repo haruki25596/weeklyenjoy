79b747890c03b4abd3a62bea60ae0b5a473ed0ea
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-08-27 15:05:43 +0000

    商品関連ページ、会員情報登録ページ作成

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,13 +1,25 @@
 class Customer::OrdersController < ApplicationController
   def index
+    @orders = current_customer.orders
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def new
+    @order = Order.new
+    
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+  end
+  
+  private
+
+  def order_params
+    params.require(:order).permit(:name,:address,:postal_code,:billing_amount,:payment_method,:status,:address_option)
   end
 end
