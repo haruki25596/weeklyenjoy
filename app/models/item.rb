14c798a5925db3f3281e68f8d0270a08ffac1be7
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-08-31 19:14:47 +0000

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,11 @@ class Item < ApplicationRecord
   has_many :ordered_items
   attachment :image
   
+  validates :genre_id, presence: true
+  validates :name, presence: true
+  validates :caption, presence: true
+  validates :tax_out_price, presence: true
+  validates :image, presence: true
+  validates :is_active, inclusion: { in: [true, false] }
+  
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -2,4 +2,13 @@ class ShippingAddress < ApplicationRecord
   
   belongs_to :customer
   
+  validates :customer_id,presence: true
+  validates :name,presence: true
+  validates :postal_code,presence: true
+  validates :address,presence: true
+  
+  def full_address
+    self.postal_code + self.address
+  end
+  
 end
